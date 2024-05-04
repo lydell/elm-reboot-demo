b81c69aeacfad0afd994d343a887f11dac8cb56a
Simon Lydell <simon.lydell@gmail.com>
2024-05-04 11:45:28 +0200

    Clean up postprocessing

--- postprocess.mjs
+++ postprocess.mjs
@@ -1,58 +1,77 @@
 export default function postprocess({ code }) {
   return (
     code
+      // This is the only string replacement that needs to be done on the compiled JS.
       // Patch the line adding the click listeners on `<a>` elements to store the listener so we can remove the listener later.
-      // TODO: Can this be avoided by clever _VirtualDom_divertHrefToApp hacks?
+      // I haven't found a way to do this with clever hacks instead.
       .replace(
         `domNode.addEventListener('click', _VirtualDom_divertHrefToApp(domNode));`,
-        `var listener = _VirtualDom_divertHrefToApp(domNode); domNode.addEventListener('click', listener); domNode.elmAf = listener;`
+        `var listener = _VirtualDom_divertHrefToApp(domNode);
+        domNode.addEventListener('click', listener);
+        domNode.elmAf = listener;`
       )
-      // Actually part of the _Platform_initialize replacement function.
+      // The rest of the replacements can be done by copy-pasting the _Platform_initialize function to the end of the IIFE and modifying it.
+      // It's just more convenient to do string replacements in this demo.
       .replace(
         `var stepper = stepperBuilder(sendToApp, model);`,
         `
-            var cleanups = [];
-            var win = _Browser_window;
-            _Browser_window = {
-                navigator: win.navigator,
-                addEventListener: function(eventName, listener) {
-                    win.addEventListener(eventName, listener);
-                    cleanups.push(function() {
-                        win.removeEventListener(eventName, listener);
-                    });
-                },
-            };
-            var virtualize = _VirtualDom_virtualize;
-            var applyPatches = _VirtualDom_applyPatches;
-            var equalEvents = _VirtualDom_equalEvents;
-            if (args && args.lastVNode) {
-                _VirtualDom_virtualize = function() {
-                    return args.lastVNode;
-                };
-                _VirtualDom_applyPatches = function(rootDomNode, oldVirtualNode, patches, eventNode) {
-                    if (patches.length !== 0) {
-                        _VirtualDom_addDomNodes(rootDomNode, oldVirtualNode, patches, eventNode);
-                    }
-                    _VirtualDom_lastDomNode = _VirtualDom_applyPatchesHelp(rootDomNode, patches);
-                    // Restore the event listeners on the <a> elements:
-                    for (const domNode of _VirtualDom_lastDomNode.getElementsByTagName('a')) {
-                        var listener = _VirtualDom_divertHrefToApp(domNode); domNode.addEventListener('click', listener); domNode.elmAf = listener;
-                    }
-                    return _VirtualDom_lastDomNode;
-                }
-                // Force all event listeners to be re-applied:
-                _VirtualDom_equalEvents = function(x, y) {
-                    return false;
-                }
+    // We'll temporarily overwrite these variables or functions.
+    var _Browser_window_backup = _Browser_window;
+    var _VirtualDom_virtualize_backup = _VirtualDom_virtualize;
+    var _VirtualDom_applyPatches_backup = _VirtualDom_applyPatches;
+    var _VirtualDom_equalEvents_backup = _VirtualDom_equalEvents;
+
+    // To be able to remove the popstate and hashchange listeners.
+    var historyListenerCleanups = [];
+    _Browser_window = {
+        navigator: _Browser_window_backup.navigator,
+        addEventListener: function(eventName, listener) {
+            _Browser_window_backup.addEventListener(eventName, listener);
+            historyListenerCleanups.push(function() {
+                _Browser_window_backup.removeEventListener(eventName, listener);
+            });
+        },
+    };
+
+    // When passing in the last rendered VNode from a previous app:
+    if (args && args.lastVNode) {
+        // Instead of virtualizing the existing DOM into a VNode, just use the one from the previous app.
+        // Html.map messes up Elm's _VirtualDom_virtualize, causing the entire thing inside the Html.map to be re-created even though it is already the correct DOM.
+        _VirtualDom_virtualize = function() {
+            return args.lastVNode;
+        };
+
+        _VirtualDom_applyPatches = function(rootDomNode, oldVirtualNode, patches, eventNode) {
+            if (patches.length !== 0) {
+                _VirtualDom_addDomNodes(rootDomNode, oldVirtualNode, patches, eventNode);
             }
-            var stepper = stepperBuilder(sendToApp, model);
-            _Browser_window = win;
-            _VirtualDom_virtualize = virtualize;
-            _VirtualDom_applyPatches = applyPatches;
-            _VirtualDom_equalEvents = equalEvents;
+            _VirtualDom_lastDomNode = _VirtualDom_applyPatchesHelp(rootDomNode, patches);
+            // Restore the event listeners on the <a> elements:
+            var aElements = _VirtualDom_lastDomNode.getElementsByTagName('a');
+            for (var i = 0; i < aElements.length; i++) {
+                var domNode = aElements[i];
+                var listener = _VirtualDom_divertHrefToApp(domNode);
+                domNode.addEventListener('click', listener);
+                domNode.elmAf = listener;
+            }
+            return _VirtualDom_lastDomNode;
+        }
+
+        // Force all event listeners to be re-applied:
+        _VirtualDom_equalEvents = function(x, y) {
+            return false;
+        }
+    }
+
+    var stepper = stepperBuilder(sendToApp, model);
+
+    // Restore the original functions and variables.
+    _Browser_window = _Browser_window_backup;
+    _VirtualDom_virtualize = _VirtualDom_virtualize_backup;
+    _VirtualDom_applyPatches = _VirtualDom_applyPatches_backup;
+    _VirtualDom_equalEvents = _VirtualDom_equalEvents_backup;
         `
       )
-      // Actually part of the _Platform_initialize replacement function.
       .replaceAll(
         `__elmWatchProgramType: { value: programType },`,
         `__elmWatchProgramType: { value: programType },
@@ -85,27 +104,30 @@ export default function postprocess({ code }) {
                     _Platform_effectsQueue = [];
 
                     // Remove Elm's event listeners. Both the ones added automatically on every <a> element, as well as the ones added by using Html.Events.
-                    for (const element of _VirtualDom_lastDomNode.getElementsByTagName('*')) {
+                    var elements = _VirtualDom_lastDomNode.getElementsByTagName('*');
+                    for (var i = 0; i < elements.length; i++) {
+                        var element = elements[i];
                         if (element.elmAf) {
                             element.removeEventListener('click', element.elmAf);
                             delete element.elmAf;
                         }
                         if (element.elmFs) {
-                            for (const key in element.elmFs) {
+                            for (var key in element.elmFs) {
                                 element.removeEventListener(key, element.elmFs[key]);
                             }
                             delete element.elmFs;
                         }
                     }
 
-                    // Remove the popstate listener.
-                    for (const cleanup of cleanups) {
-                        cleanup();
+                    // Remove the popstate and hashchange listeners.
+                    for (var i = 0; i < historyListenerCleanups.length; i++) {
+                        historyListenerCleanups[i]();
                     }
 
                     // Clear these new things we've added.
                     _VirtualDom_lastVNode = null;
                     _VirtualDom_lastDomNode = null;
+                    historyListenerCleanups = [];
 
                     return toReturn;
                 }
@@ -115,6 +137,7 @@ export default function postprocess({ code }) {
       .replace(
         `}(this));`,
         `
+// Keep track of the last VNode rendered so we can pass it to the next app later.
 var _VirtualDom_lastVNode = null;
 function _VirtualDom_diff(x, y)
 {
@@ -124,6 +147,7 @@ function _VirtualDom_diff(x, y)
 	return patches;
 }
 
+// Keep track of the reference to the latest root DOM node so we can perform cleanups in it later.
 var _VirtualDom_lastDomNode = null;
 function _VirtualDom_applyPatches(rootDomNode, oldVirtualNode, patches, eventNode)
 {
